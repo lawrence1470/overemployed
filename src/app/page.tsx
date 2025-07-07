2c0f4c64a3ca8a090e03ac98022d6c4a7845ff29
lawrence nicastro <lawrence.nicastro1@gmail.com>
2025-07-07 17:37:19 -0400

    test'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from "~/app/_components/hero-section";
 import { MoonlightingSection } from "~/app/_components/moonlighting-section";
+import { SohamSection } from "./_components/soham-section";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <MoonlightingSection />
+      <SohamSection />
     </main>
   );
 }
